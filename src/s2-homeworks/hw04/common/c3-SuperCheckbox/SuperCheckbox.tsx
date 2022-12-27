ba0ae34238f03d05cd60d6b9fc4a5ade13033df1
AlucarD <skoriy12990@gmail.com>
2022-12-27 14:37:25 +0300

    Homework 4 change onChangeChecked function

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-      onChange && onChange(e)
+      onChangeChecked && onChangeChecked(e.currentTarget.checked);
       // задачка на написание онченджа
 
     }
