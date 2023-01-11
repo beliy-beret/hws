589e8764bc85387308f39bf3b015e7f7affef9f3
AlucarD <skoriy12990@gmail.com>
2023-01-11 21:58:23 +0300

    Homework 6

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import s from './App.module.css';
+import HW6 from "../s2-homeworks/hw06/HW6";
 import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
     return (
         <div className={s.App}>
-            {/*<HW1/>
-            раскомментировать по ходу выполнения
-            <Divider />
-            <HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
-
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
-            <HW5/>
+          <HW5/>
+          <HW6 />
         </div>
     )
 }
--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -4,6 +4,7 @@ import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
+import {Container, Divider} from "@mui/material";
 
 /*
  * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
@@ -19,40 +20,45 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', ''))
     }
 
     return (
         <div id={'hw6'}>
-            <div className={s2.hwTitle}>Homework #6</div>
+            <Container maxWidth={'xl'}>
+                <div className={s2.hwTitle}>Homework #6</div>
+            </Container>
+            <Divider />
 
             {/*демонстрация возможностей компоненты:*/}
-            <div className={s2.hw}>
-                <div className={s.editableSpanContainer}>
-                    <SuperEditableSpan
-                        id={'hw6-spanable-input'}
-                        value={value}
-                        onChangeText={setValue}
-                        spanProps={{
-                            id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
-                        }}
-                    />
-                </div>
+            <Container maxWidth={'xl'}>
+                <div className={s2.hw}>
+                    <div className={s.editableSpanContainer}>
+                        <SuperEditableSpan
+                          id={'hw6-spanable-input'}
+                          value={value}
+                          onChangeText={setValue}
+                          spanProps={{
+                              id: 'hw6-editable-span',
+                              defaultText: 'enter text...',
+                          }}
+                        />
+                    </div>
 
-                <div className={s.buttonsContainer}>
-                    <SuperButton id={'hw6-save'} onClick={save}>
-                        Save to ls
-                    </SuperButton>
-                    <SuperButton
-                        id={'hw6-restore'}
-                        onClick={restore}
-                        xType={'secondary'}
-                    >
-                        Get from ls
-                    </SuperButton>
+                    <div className={s.buttonsContainer}>
+                        <SuperButton id={'hw6-save'} onClick={save}>
+                            Save to ls
+                        </SuperButton>
+                        <SuperButton
+                          id={'hw6-restore'}
+                          onClick={restore}
+                          xType={'secondary'}
+                        >
+                            Get from ls
+                        </SuperButton>
+                    </div>
                 </div>
-            </div>
+            </Container>
         </div>
     )
 }
