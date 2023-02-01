8703806839e2a7305c966131fba7f97a8095d414
AlucarD <skoriy12990@gmail.com>
2023-02-01 10:51:51 +0300

    Home work 9, refactor logic

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,7 +1,4 @@
 import HW5 from "../s2-homeworks/hw05/HW5";
-import HW6 from "../s2-homeworks/hw06/HW6";
-import HW7 from '../s2-homeworks/hw07/HW7';
-import HW8 from "../s2-homeworks/hw08/HW8";
 import s from './App.module.css';
 
 function App() {
@@ -9,9 +6,6 @@ function App() {
         <div className={s.App}>
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5 />
-            <HW6 />
-            <HW7 />
-            <HW8 />
         </div>
     )
 }
--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { Container, Divider } from '@mui/material'
+
 import Clock from './Clock'
+import React from 'react'
 import s2 from '../../s1-main/App.module.css'
 
 /*
@@ -12,11 +14,15 @@ import s2 from '../../s1-main/App.module.css'
 const HW9 = () => {
     return (
         <div id={'hw9'}>
-            <div className={s2.hwTitle}>Homework #9</div>
-
-            <div className={s2.hw}>
-                <Clock />
-            </div>
+            <Container maxWidth={'xl'}>
+                <div className={s2.hwTitle}>Homework #9</div>
+            </Container>
+            <Divider />
+            <Container maxWidth={'xl'} sx={{ pb: '2rem' }}>
+                <div className={s2.hw}>
+                    <Clock />
+                </div>
+            </Container>
         </div>
     )
 }
