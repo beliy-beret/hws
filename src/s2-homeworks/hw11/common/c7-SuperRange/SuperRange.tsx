fdd65bf758339b2110aef8bd1ae89f8786274f0d
AlucarD <skoriy12990@gmail.com>
2023-02-16 17:28:27 +0300

    add slider logic

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
+
+import React from 'react'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                maxWidth: '180px'
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
