7dc6de1070adbcf105ff43a86166cbbd3e18d466
AlucarD <skoriy12990@gmail.com>
2023-03-02 14:32:22 +0300

    refactoring send function

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -39,29 +39,28 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 // дописать                
-                setText('...всё ок) код 200 - обычно означает что скорее всего всё ок)')
-                setInfo('')    
+                setText(res.data.errorText)
+                setInfo(res.data.info)    
             })
             .catch((e) => {
-                // дописать
-                if(x === false) {
-                    setCode('Ошибка 400!')
-                    setImage(error400)
-                    setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что - то не то на бэк!')
-                    setInfo('')
+                console.log(e);
+                if(e.response.data){
+                    setText(e.response.data.errorText)
+                    setInfo(e.response.data.info)
+                    if(x === false){
+                        setCode('500')
+                        setImage(error500)
+                    }
+                    if(x === undefined) {
+                        setCode('400')
+                        setImage(error400)
+                    }
+                    return
                 }
-                if (x === undefined) {
-                    setCode('Ошибка 500!')
-                    setImage(error500)
-                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что - то сломалось на сервере, например база данных)')
-                    setInfo('')
-                }
-                if (x === null) {
-                    setCode('Error!')
-                    setImage(errorUnknown)
-                    setText('Network Error AxiosError')
-                    setInfo('')
-                }               
+                setImage(errorUnknown)
+                setCode('Error!')
+                setText(e.message)
+                setInfo(e.name)         
             })
     }
 
@@ -79,7 +78,7 @@ const HW13 = () => {
                             onClick={send(true)}
                             xType={'secondary'}
                             // дописать
-                            disabled={!!info}
+                            disabled={info === '...loading'}
                         >
                             Send true
                         </SuperButton>
@@ -88,7 +87,7 @@ const HW13 = () => {
                             onClick={send(false)}
                             xType={'secondary'}
                             // дописать
-                            disabled={!!info}
+                            disabled={info === '...loading'}
                         >
                             Send false
                         </SuperButton>
@@ -97,7 +96,7 @@ const HW13 = () => {
                             onClick={send(undefined)}
                             xType={'secondary'}
                             // дописать
-                            disabled={!!info}
+                            disabled={info === '...loading'}
                         >
                             Send undefined
                         </SuperButton>
@@ -106,7 +105,7 @@ const HW13 = () => {
                             onClick={send(null)} // имитация запроса на не корректный адрес
                             xType={'secondary'}
                             // дописать
-                            disabled={!!info}
+                            disabled={info === '...loading'}
                         >
                             Send null
                         </SuperButton>
